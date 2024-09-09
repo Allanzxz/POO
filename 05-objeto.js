3c3caa765156660c9491dea5bf91342dfef195c3
David <davidasafe07@gmail.com>
2024-09-09 11:25:34 -0400

    add poo objeto

--- 05-objeto.js
+++ 05-objeto.js
@@ -12,5 +12,15 @@ class Pessoa {
   }
 }
 // Objeto/Instancia "pessoal" criada a partir da classe pessoa
-const pessoa1 = new Pessoa('Allan', 16, 1.85, 78.0);
+const pessoa1 = new Pessoa("Allan", 16, 1.85, 78.0);
 pessoa1.falar(); // Chamada do método falar() da classe Pessoa
+
+
+// exercicio
+// Outro objeto/instancia "!pessoal2" criada a partir da mesma classe Pessoa
+
+const pessoa2 = new Pessoa('Ana Luiza', 23, 1.72, 62);
+pessoa2.falar(); // Chamada do método falar() da classe Pessoa
+
+const pessoa3 = new Pessoa('Matheo', 13, 1.50, 82);
+pessoa3.falar(); // Chamada do método falar() da classe Pessoa
