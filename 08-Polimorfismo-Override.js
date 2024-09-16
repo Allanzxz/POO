df494706318558d4cd965c2df0c85409e52e386d
David <davidasafe07@gmail.com>
2024-09-16 10:29:28 -0400

    finished exercicio de polimorfismo

--- 08-Polimorfismo-Override.js
+++ 08-Polimorfismo-Override.js
@@ -1,69 +1,70 @@
 // Classe base Personagem
 class Personagem {
-    constructor(nome, vida) {
-        this.nome = nome;
-        this.vida = vida;
-    }
-    atacar() {
-        console.log(`${this.nome} realiza um ataque básico!`);
-    }
-    receberDano(dano) {
-        this.vida -= dano;
-        console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`);
-    }
+  constructor(nome, vida) {
+    this.nome = nome;
+    this.vida = vida;
+  }
+  atacar() {
+    console.log(`${this.nome} realiza um ataque básico!`);
+  }
+  receberDano(dano) {
+    this.vida -= dano;
+    console.log(
+      `${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`
+    );
+  }
 }
 
 // Classe Guerreiro que herda de personagem
 
 class Guerreiro extends Personagem {
-    constructor(nome, vida, arma) {
-        super(nome, vida);
-        this.arma = arma;
-    }
+  constructor(nome, vida, arma) {
+    super(nome, vida);
+    this.arma = arma;
+  }
 
-    atacar() {
-        super.atacar();
-        console.log(`${this.nome} realiza um ataque com a ${this.arma}`);
-    }
+  atacar() {
+    super.atacar();
+    console.log(`${this.nome} realiza um ataque com a ${this.arma}`);
+  }
 }
 
 // Classe Mago que herda de Personagem
 
 class Mago extends Personagem {
-    constructor(nome, vida, feitico) {
-        super(nome, vida);
-        this.feitico = feitico;
-    }
+  constructor(nome, vida, feitico) {
+    super(nome, vida);
+    this.feitico = feitico;
+  }
 
-    atacar() {
-        super.atacar();
-        console.log(`${this.nome} lança ${this.feitico}`);
-    }
+  atacar() {
+    super.atacar();
+    console.log(`${this.nome} lança ${this.feitico}`);
+  }
 }
 
 // Classe Arqueiro que herda de Personagem
-    class Arqueiro extends Personagem {
-    constructor (nome, vida, tipoFlecha) {
-        super(nome, vida);
-        this.tipoFlecha = tipoFlecha;
-    }
+class Arqueiro extends Personagem {
+  constructor(nome, vida, tipoFlecha) {
+    super(nome, vida);
+    this.tipoFlecha = tipoFlecha;
+  }
 }
- atacar() ;{
-        super.atacar();
-        console.log(`${this.nome} dispara a flecha ${this.tipoFlecha}`);
-    }
+atacar();
+{
+  super.atacar();
+  console.log(`${this.nome} dispara a flecha ${this.tipoFlecha}`);
+}
 
-    // Criando instâncias de Guerreiro, Mago e Arqueiro
+// Criando instâncias de Guerreiro, Mago e Arqueiro
 const guerreiro = new Guerreiro("Arthur", 100, "espada");
-guerreiro.atacar (); // Saída: Arthur ataca com a espada!
-guerreiro. receberDano(20); // Saída: Arthur recebeu 20 de dano. Vida restante: 80
+guerreiro.atacar(); // Saída: Arthur ataca com a espada!
+guerreiro.receberDano(20); // Saída: Arthur recebeu 20 de dano. Vida restante: 80
 
 const mago = new Mago("Merlin", 80, "bola de fogo");
 mago.atacar(); // Saída: Merlin lança bola de fogo!
-mago.receberDano (15); // Saida: Merlin recebeu 15 de dano. Vida restante: 65
+mago.receberDano(15); // Saida: Merlin recebeu 15 de dano. Vida restante: 65
 
-const arqueiro = new Arqueiro ("Legolas", 90, "explosiva");
+const arqueiro = new Arqueiro("Legolas", 90, "explosiva");
 arqueiro.atacar(); // Saída: Legolas dispara flechas explosiva!
-arqueiro.receberDano (10); // Saida: Legolas receber de dano. Vida restante: 80
-       
-    
+arqueiro.receberDano(10); // Saida: Legolas receber de dano. Vida restante: 80
---
+++ 09-exercicio-polimorfismo-override.js
@@ -0,0 +1,54 @@
+class Cidade {
+  constructor(nome, populacao, area, fundacao, densidade) {
+    this.nome = nome;
+    this.populacao = populacao;
+    this.area = area;
+    this.fundacao = fundacao;
+    this.densidade = densidade;
+  }
+  falar() {
+    console.log(`Essa é a cidade de ${this.nome}!`);
+  }
+  mostrarInfo() {
+    console.log(`A população da cidade é de ${this.populacao}!`);
+  }
+}
+
+// Classe distrito que herda de Cidade
+class Distrito extends Cidade {
+  constructor(nome, populacao, area, fundacao, densidade, regiao, prefeito) {
+    super(nome, populacao, area, fundacao, densidade);
+    this.regiao = regiao
+    this.prefeito = prefeito
+  }
+  falar() {
+    console.log(`Essa é o distrito de ${this.nome}!`);
+  }
+  mostrarInfo() {
+    console.log(`A população do distrito é de ${this.populacao} habitantes!`);
+  }
+}
+
+// Classe vilarejo que herda de Cidade
+class Vilarejo extends Cidade {
+  constructor(nome, populacao, area, fundacao, densidade, rio, aeroporto) {
+    super(nome, populacao, area, fundacao, densidade);
+    this.rio = rio
+    this.aeroporto = aeroporto;
+  }
+  falar() {
+    console.log(`Essa é o vilarejo de ${this.nome}!`);
+  }
+  mostrarInfo() {
+    console.log(`A população do vilarejo é de ${this.populacao} habitantes!`);
+  }
+}
+
+// Criando instâncias de Distrito e Vilarejo
+const distrito = new Distrito("Mimoso", 637);
+distrito.falar(); // Saída: Essa é o distrito de Mimoso!
+distrito.mostrarInfo(); // Saída: A população do distrito é de 637 habitantes!
+
+const vilarejo = new Vilarejo("Bom Alegre", 159);
+vilarejo.falar(); // Saída: Essa é o vilarejo de Bom Alegre!
+vilarejo.mostrarInfo(); // Saída: A população do vilarejo é de 159 habitantes!
